5b6ce81a8cb1964c656bf6bd7006ee5219a158f0
Lucas <lucasaraujo1964@gmail.com>
2024-05-28 11:40:56 -0300

    feat: added products filter/product list

--- caputeeno/src/components/filter-by-priority.tsx
+++ caputeeno/src/components/filter-by-priority.tsx
@@ -39,6 +39,7 @@ const PriorityFilter = styled.ul`
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
   border-radius: 4px;
   padding: 12px 16px;
+  z-index: 999;
 
   list-style: none;
   
--- caputeeno/src/components/primary-input.tsx
+++ caputeeno/src/components/primary-input.tsx
@@ -30,13 +30,14 @@ const InputContainer = styled.div`
 `
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-
+  value: string
+  handleChange: (value: string) => void
 }
 
 export function PrimaryInputWSearchIcon(props: InputProps) {
   return (
     <InputContainer>
-      <PrimaryInput {...props} />
+      <PrimaryInput onChange={(event) => props.handleChange(event.target.value)} {...props} />
       <SearchIcon />
     </InputContainer>
   )
