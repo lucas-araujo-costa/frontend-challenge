da058e584bf7d886ba6df05edb5497e95658c777
Lucas <lucasaraujo1964@gmail.com>
2024-06-14 16:40:20 -0300

    refactor: separating styling from cart component

--- caputeeno/src/app/cart/page.tsx
+++ caputeeno/src/app/cart/page.tsx
@@ -1,100 +1,14 @@
 "use client"
 
-import { BackBtn } from "@/components/back-button";
-import { CartItem } from "@/components/cart/cart-item";
-import { DefaultPageLayout } from "@/components/default-page-layout";
-import { Divider } from "@/components/divider";
+import { BackBtn } from "@/components/BackBtn";
+import { DefaultPageLayout } from '@/layouts/DefaultPageLayout';
+
 import { useLocalStorage } from "@/hooks/useLocalStorage";
-import { ProductInCart } from "@/types/product";
-import { formatPrice } from "@/utils/format-price";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  gap: 32px;
-
-  @media(min-width: ${props => props.theme.desktopBreakpoint}) {
-    flex-direction: row;
-  }
-`
-
-const CartListContainer = styled.div`
-
-  h3 {
-    font-size: 24px;
-    font-weight: 500;
-    text-transform: uppercase;
-    color: var(--text-dark-2);
-    line-height: 150%;
-    margin-top: 24px;
-  }
-
-  p {
-    font-weight: 300;
-    font-size: 16px;
-    line-height: 150%;
-    color: var(--text-dark-2);
-
-    span {
-      font-weight: 600;
-    }
-  }
-`
-
-const CartList = styled.ul`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 16px;
-  margin-top: 24px;
-`
-
-const CartResultContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-
-  min-width: 352px;
-  padding: 16px 24px;
-
-  background: #fff;
-
-  h3 {
-    font-weight: 600;
-    font-size: 20px;
-    color: var(--text-dark-2);
-    text-transform: uppercase;
-    margin-bottom: 30px;
-  }
-`
-
-const TotalItem = styled.div<{ isBold: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-
-  font-weight: ${props => props.isBold ? '600' : '400'};
-  font-size: 16px;
-  line-height: 150%;
-
-  margin-bottom: 12px;
-`
-
-const ShopBtn = styled.button`
-  color: white;
-  border-radius: 4px;
-  background: var(--sucess-color);
-  padding: 12px;
-  width: 100%;
-  border: none;
-  margin-top: 40px;
-  cursor: pointer;
-`
+import { ProductInCart } from "@/types/Product";
+import { formatPrice } from "@/utils/formatPrice";
+import { CartList, CartListContainer, CartResultContainer, Container, ShopBtn, TotalItem } from "./styles";
+import { CartItem } from "@/components/Cart";
+import { Divider } from "@/layouts/divider";
 
 export default function CartPage() {
   const { value, updateLocalStorage } = useLocalStorage<ProductInCart[]>("cart-items", [])
---
+++ caputeeno/src/app/cart/styles.ts
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 32px;
+
+  @media(min-width: ${props => props.theme.desktopBreakpoint}) {
+    flex-direction: row;
+  }
+`
+
+export const CartListContainer = styled.div`
+
+  h3 {
+    font-size: 24px;
+    font-weight: 500;
+    text-transform: uppercase;
+    color: var(--text-dark-2);
+    line-height: 150%;
+    margin-top: 24px;
+  }
+
+  p {
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 150%;
+    color: var(--text-dark-2);
+
+    span {
+      font-weight: 600;
+    }
+  }
+`
+
+export const CartList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
+  margin-top: 24px;
+`
+
+export const CartResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  min-width: 352px;
+  padding: 16px 24px;
+
+  background: #fff;
+
+  h3 {
+    font-weight: 600;
+    font-size: 20px;
+    color: var(--text-dark-2);
+    text-transform: uppercase;
+    margin-bottom: 30px;
+  }
+`
+
+export const TotalItem = styled.div<{ isBold: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  font-weight: ${props => props.isBold ? '600' : '400'};
+  font-size: 16px;
+  line-height: 150%;
+
+  margin-bottom: 12px;
+`
+
+export const ShopBtn = styled.button`
+  color: white;
+  border-radius: 4px;
+  background: var(--sucess-color);
+  padding: 12px;
+  width: 100%;
+  border: none;
+  margin-top: 40px;
+  cursor: pointer;
+`
